7055865d3d720f4a94d105522254e8adddb3a52d
Benjamin Normann Hansen <normannesigns@outlook.com>
2019-03-26 10:59:52 +0100

    New default size

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const { app, BrowserWindow } = electron
 
 let win = null
 function createWindow () {
-  win = new BrowserWindow({ width: 300, minWidth: 300, height: 500, minHeight: 500, frame: false, transparent: true, webPreferences: {nodeIntegration: true}, resizable: false})
+  win = new BrowserWindow({ width: 350, minWidth: 350, height: 500, minHeight: 500, frame: false, transparent: true, webPreferences: {nodeIntegration: true}, resizable: false})
   win.loadURL('http://localhost:3000/')
   //win.webContents.openDevTools()
   
