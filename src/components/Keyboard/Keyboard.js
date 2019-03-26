e327f96d66bc67bb9fbc2054b8706989bfb1257f
Benjamin Normann Hansen <normanndesigns@outlook.com>
2019-03-26 19:23:58 +0100

    Added a function that gets the button value when pressed plus made a loop instead of repeating myself

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -3,37 +3,36 @@ import {Display} from "../Display/Display";
 import {MainColors} from "../Color/Color";
 
 class Keyboard extends Component {
+  constructor(){
+    super();
+    this.state = {
+      keyTypes: [
+        "c", "+/−", "%", "÷",
+        "7", "8", "9", "×",
+        "4", "5", "6", "−",
+        "1", "2", "3", "+",
+        "0", "•", "="
+    ]
+    }
+  }
   render() {
+    function addToScreen(data) {
+      console.log(data);
+    }
+
     let border = {
       borderColor: MainColors.dark,
       borderStyle: 'solid',
       borderWidth: 1
     };
-    function AddToScreen(){
-      
+
+    function Key(props) {
+      return props.keytype !== "0" ? <li className="Smallbuttons" onClick={((e) => addToScreen(props.keytype))} style={border}>{props.keytype}</li> : <li className="Largebuttons" onClick={((e) => addToScreen(props.keytype))} style={border}>{props.keytype}</li>;
     }
     return (
         <div className="keyboard">
             <ul>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>C</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>+/−</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>%</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>÷</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>7</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>8</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>9</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>×</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>4</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>5</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>6</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>−</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>1</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>2</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>3</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>+</li>
-              <li className="Largebuttons" onClick={AddToScreen} style={border}>0</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>•</li>
-              <li className="Smallbuttons" onClick={AddToScreen} style={border}>=</li>
+              {this.state.keyTypes.map((key) => <Key key={"keyID" + key} keytype={key} />)}
             </ul>
         </div>
     );
