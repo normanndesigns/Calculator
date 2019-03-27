0191e76edfcddd79affab81424110d32e26a4cc0
Benjamin Normann Hansen <normanndesigns@outlook.com>
2019-03-27 16:01:14 +0100

    Moved functions outside render

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -5,6 +5,8 @@ import {MainColors} from "../Color/Color";
 class Keyboard extends Component {
   constructor(){
     super();
+    this.Key = this.Key.bind(this)
+    this.addToScreen = this.addToScreen.bind(this)
     this.state = {
       keyTypes: [
         "c", "+/−", "%", "÷",
@@ -15,24 +17,17 @@ class Keyboard extends Component {
     ]
     }
   }
+  addToScreen(data) {
+    console.log(data);
+  }
+  Key(props) {
+    return props.keytype !== "0" ? <li className="Smallbuttons" onClick={((e) => this.addToScreen(props.keytype))} style={{ borderColor: MainColors.dark, borderStyle: 'solid', borderWidth: 1 }}>{props.keytype}</li> : <li className="Largebuttons" onClick={((e) => this.addToScreen(props.keytype))} style={{ borderColor: MainColors.dark, borderStyle: 'solid', borderWidth: 1 }}>{props.keytype}</li>;
+  }
   render() {
-    function addToScreen(data) {
-      console.log(data);
-    }
-
-    let border = {
-      borderColor: MainColors.dark,
-      borderStyle: 'solid',
-      borderWidth: 1
-    };
-
-    function Key(props) {
-      return props.keytype !== "0" ? <li className="Smallbuttons" onClick={((e) => addToScreen(props.keytype))} style={border}>{props.keytype}</li> : <li className="Largebuttons" onClick={((e) => addToScreen(props.keytype))} style={border}>{props.keytype}</li>;
-    }
     return (
         <div className="keyboard">
             <ul>
-              {this.state.keyTypes.map((key) => <Key key={"keyID" + key} keytype={key} />)}
+              {this.state.keyTypes.map((key) => <this.Key key={"keyID" + key} keytype={key} />)}
             </ul>
         </div>
     );
