ee11ebd316d37b5b76e86796d073d8c8e95a2dda
Benjamin Normann Hansen <normanndesigns@outlook.com>
2019-03-30 13:00:07 +0100

    Implemented functions to switch between calculator and color mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import Menu from "./components/Menu/Menu";
 import Keyboard from "./components/Keyboard/Keyboard";
-import {MainColors} from "./components/Color/Color";
+import Color, { MainColors } from "./components/Color/Color";
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.forceRefresh = this.forceRefresh.bind(this)
+    this.reverseShow = this.reverseShow.bind(this)
+    this.keyboardOrColor = this.keyboardOrColor.bind(this)
+    this.state = {
+       showkeyboard: true
+    }
+  }
+  forceRefresh() {
+    this.forceUpdate();
+  }
+  keyboardOrColor(){
+    if(this.state.showkeyboard){
+      return <Keyboard reverseShow={this.reverseShow} />
+    }else{
+      return <Color reverseShow={this.reverseShow} forceRefresh={this.forceRefresh} />
+    }
+  }
+  reverseShow(){
+    this.setState({
+      showkeyboard: !this.state.showkeyboard
+    })
+  }
   render() {
-    return (
+  return (
       <div className="App">
         <div className="global-wrapper" style={{"backgroundColor": MainColors.light}}>
           <Menu />
-          <Keyboard />
+          {this.keyboardOrColor()}
         </div>
       </div>
     );
