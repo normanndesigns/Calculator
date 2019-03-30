f45bffac7e09129a7f1b57efd9ecbc0e75bff008
Benjamin Normann Hansen <normanndesigns@outlook.com>
2019-03-30 12:25:36 +0100

    Deleted an unnecessary comment and disabled electron warnings

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const electron = require('electron')
 const path = require('path')
-
+delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
 const { app, BrowserWindow } = electron
 
 let win = null
 function createWindow () {
   win = new BrowserWindow({ width: 350, minWidth: 350, height: 500, minHeight: 500, frame: false, transparent: true, webPreferences: {nodeIntegration: true}, resizable: false})
   win.loadURL('http://localhost:3000/')
-  //win.webContents.openDevTools()
-  
 }
 app.on('ready', createWindow)
 
