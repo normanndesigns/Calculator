83860e63bfc199fa0df88b92a0409088dee6218e
Benjamin Normann <normanndesigns@outlook.com>
2019-04-02 23:35:17 +0200

    Changed readfile and writefile to writeFileSync and readFileSync

--- main.js
+++ main.js
@@ -5,21 +5,13 @@ const ipc = electron.ipcMain;
 delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
 const { app, BrowserWindow } = electron
-
+var settings = JSON.parse(fs.readFileSync(__dirname + '/src/settings.json'))
 let win = null
 function createWindow () {
   win = new BrowserWindow({ width: 350, minWidth: 350, height: 500, minHeight: 500, frame: false, transparent: true, webPreferences: {nodeIntegration: true}, resizable: false})
-    fs.readFile( __dirname + '/src/settings.json', function (err, data) {
-      if (err) {
-        throw err; 
-      }
-      var colorData = JSON.parse(data)
-      win.loadURL('http://localhost:3000?dark='+ colorData.dark + "&light=" + colorData.light)
-    });
+    win.loadURL('http://localhost:3000?dark=' + settings.dark + '&light=' + settings.light)
     ipc.on('colors', (event, args) => {
-      fs.writeFile (__dirname + '/src/settings.json', JSON.stringify(args), function(err) {
-        if (err) throw err;
-        }
+      fs.writeFileSync(__dirname + '/src/settings.json', JSON.stringify(args)
     );
       event.returnValue = 'Color Changed';
     });
