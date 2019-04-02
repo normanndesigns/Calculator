4616323179c0893374a7a767946ec9a539308792
Benjamin Normann <normanndesigns@outlook.com>
2019-04-02 21:10:33 +0200

    Implemented so now it gets the default color from settings.json plus when color is changed, it's saved for next time

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const electron = require('electron')
+const fs = require('fs')
 const path = require('path')
+const ipc = electron.ipcMain;
 delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
 const { app, BrowserWindow } = electron
@@ -7,7 +9,19 @@ const { app, BrowserWindow } = electron
 let win = null
 function createWindow () {
   win = new BrowserWindow({ width: 350, minWidth: 350, height: 500, minHeight: 500, frame: false, transparent: true, webPreferences: {nodeIntegration: true}, resizable: false})
-  win.loadURL('http://localhost:3000/')
+    fs.readFile( __dirname + '/src/settings.json', function (err, data) {
+      if (err) {
+        throw err; 
+      }
+      var colorData = JSON.parse(data)
+      win.loadURL('http://localhost:3000?dark='+ colorData.dark + "&light=" + colorData.light)
+    });
+    ipc.on('colors', (event, args) => {
+      fs.writeFile (__dirname + '/src/settings.json', JSON.stringify(args), function(err) {
+        if (err) throw err;
+        }
+    );
+      event.returnValue = 'Color Changed';
+    });
 }
 app.on('ready', createWindow)
-
